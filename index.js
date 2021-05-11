a044130d64cc6a2d4a07c63163c673e76e4dcb88
Lucas Azevedo <lg.azevedo95@gmail.com>
2021-05-10 23:51:33 -0300

    apenas dizendo que amo a giulia

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! This was updated automatically!');
+  res.send('Eu amo minha Giu Giu');
 });
 
 var server = app.listen(3000, function () {
